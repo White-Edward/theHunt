d2594547096f7b154d15035707f42703beb474dd
Ann Lloyd <annj456@gmail.com>
2016-01-30 16:26:35 -0700

    1/30/2016 - Added OldCell Class

---
+++ theHunt/src/byui/cit260/theHunt/model/OldCell.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.theHunt.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author Ann
+ */
+public class OldCell implements Serializable {
+    private String itemType;
+    private boolean used;
+
+    public OldCell() {
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.itemType);
+        hash = 17 * hash + (this.used ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final OldCell other = (OldCell) obj;
+        if (this.used != other.used) {
+            return false;
+        }
+        if (!Objects.equals(this.itemType, other.itemType)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "OldCell{" + "itemType=" + itemType + ", used=" + used + '}';
+    }
+    
+    
+}
