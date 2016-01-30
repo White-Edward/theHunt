552ed76f9313799ad7fe646dcf90e99bd666a6b3
Ryan <Ryan@Funderburk>
2016-01-30 01:42:44 -0600

    1/30/16 Added Game class

--- theHunt/src/thehunt/TheHunt.java
+++ theHunt/src/thehunt/TheHunt.java
@@ -5,6 +5,7 @@
  */
 package thehunt;
 
+import byui.cit260.theHunt.model.Game;
 import byui.cit260.theHunt.model.Player;
 
 /**
@@ -16,6 +17,17 @@ public class TheHunt {
     /**
      * @param args the command line arguments
      */
+    void RyanTestModules() {
+    Game newGame = new Game();
+    
+    newGame.setName("Jim");
+    newGame.setWelcomeMessage("Hi Jim, welcome to The Hunt!");
+    
+    String gameInfo = newGame.toString();
+    System.out.println(gameInfo);
+    }
+            
+    
     public static void main(String[] args) {
         Player playerOne = new Player();
         
@@ -23,6 +35,8 @@ public class TheHunt {
         
         String playerInfo = playerOne.toString();
         System.out.println(playerInfo);
+        
+
     }
     
 }
