5427f83c81cd535de3b48481203cd919eee46f3b
Ann Lloyd <annj456@gmail.com>
2016-02-19 18:09:11 -0700

    2/19/2016 -  Created GameMenuView class and added a dummy displayMenu function

---
+++ theHunt/src/byui/cit260/theHunt/view/GameMenuView.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.theHunt.view;
+
+/**
+ *
+ * @author Ann
+ */
+public class GameMenuView {
+    
+    void displayMenu() {
+        System.out.println("\n*** displayMenu stub function called***");
+    }
+}
