18530d4c8b319b27f3a8c495d2ec95977f87f11e
Ann Lloyd <annj456@gmail.com>
2016-02-24 22:45:38 -0700

    2/24/2016 -  Removed algorithm comment from calculateTwoTrains function

--- theHunt/src/byui/cit260/theHunt/control/QuestionControl.java
+++ theHunt/src/byui/cit260/theHunt/control/QuestionControl.java
@@ -12,20 +12,6 @@ package byui.cit260.theHunt.control;
 public class QuestionControl {
     
     public double calculateTwoTrains(double milesTravelledTrainOne, double milesTravelledTrainTwo, double milesPerHourTrainOne, double milesPerHourTrainTwo) {
-//    BEGIN
-//    IF (milesTravelledTrainOne < 1 OR milesTravelledTrainOne > 200) THEN
-//            RETURN -1
-//    IF (milesTravelledTrainTwo < 1 OR milesTravelledTrainTwo > 200) THEN
-//            RETURN -1
-//    IF (milesPerHourTrainOne < 1 OR milesPerHourTrainOne > 120) THEN
-//            RETURN -2
-//    IF (milesPerHourTrainTwo < 1 OR milesPerHourTrainTwo > 120) THEN
-//            RETURN -2
-//    hoursTrainOne = milesTravelledTrainOne / milesPerHourTrainOne
-//    hoursTrainTwo = milesTravelledTrainTwo / milesPerHourTrainTwo
-//    hours = ROUND(hoursTrainOne + hoursTrainTwo)
-//    RETURN hours
-//    END
         if (milesTravelledTrainOne < 1 || milesTravelledTrainOne > 200) {
             return -1;
         }
