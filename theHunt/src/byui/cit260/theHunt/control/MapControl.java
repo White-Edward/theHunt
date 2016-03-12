f4466f52a4dd4c4c76b1fc930430d41941fb311f
Ann Lloyd <annj456@gmail.com>
2016-03-11 23:34:20 -0700

    3/11/2016 - Moved createQuestions from MapControl to QuestionControl

--- theHunt/src/byui/cit260/theHunt/control/MapControl.java
+++ theHunt/src/byui/cit260/theHunt/control/MapControl.java
@@ -24,53 +24,13 @@ public class MapControl {
         
         // Create the map
         Map map = new Map(5,5);
-        
-        // Create a list of the different questions in the game
-        ArrayList<Question> questions = createQuestions();
-        
+              
         // Assign the different questions to locations in the map
-        assignQuestionsToLocations(map,questions);
+        assignQuestionsToLocations(map,TheHunt.getCurrentGame().getQuestions());
         
         // Assign the different items to locations in the map
         assignItemsToLocations(map,TheHunt.getCurrentGame().getItems());
         return map;
-    }
-    private static ArrayList<Question> createQuestions() {
-        ArrayList<Question> questions = new ArrayList();
-        
-        //Question[] questions = new Question[QuestionType.values().length + Constants.RIDDLES.length];
-        
-        Question empty = new Question();
-        empty.setQuestionType(QuestionType.empty);
-        empty.setRiddle("No riddle here, keep searching");
-        questions.add(empty);
-        
-        for (int i = 0; i < Constants.RIDDLES[0].length; i++) {
-            Question riddle = new Question();
-            riddle.setQuestionType(QuestionType.riddle);
-            riddle.setRiddle(Constants.RIDDLES[0][i]);
-            riddle.setAnswer(Constants.RIDDLES[1][i]);
-            questions.add(riddle);
-        }
-        
-        Question water = new Question();
-        water.setQuestionType(QuestionType.water);
-        water.setHasWaterSquare(true);
-        questions.add(water);
-        
-        Question train = new Question();
-        train.setQuestionType(QuestionType.train);
-        train.setRiddle("train");
-        train.setHasTwoTrainSquare(true);
-        questions.add(train);
-
-        Question teaspoon = new Question();
-        teaspoon.setQuestionType(QuestionType.teaspoon);
-        teaspoon.setRiddle("teaspoon");
-        teaspoon.setHasTeaspoonSquare(true);
-        questions.add(teaspoon);
-        
-        return questions;
     }
     
     private static void assignQuestionsToLocations(Map map, ArrayList<Question> questions) {
