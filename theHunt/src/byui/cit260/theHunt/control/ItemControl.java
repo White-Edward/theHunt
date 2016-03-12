cfe9ab3ef31e43e737be631a20dac8a8cbb4e484
Ann Lloyd <annj456@gmail.com>
2016-03-11 23:34:42 -0700

    3/11/2016 - Moved createItems from GameControl to ItemControl

--- theHunt/src/byui/cit260/theHunt/control/ItemControl.java
+++ theHunt/src/byui/cit260/theHunt/control/ItemControl.java
@@ -5,11 +5,49 @@
  */
 package byui.cit260.theHunt.control;
 
+import byui.cit260.theHunt.model.Constants;
+import byui.cit260.theHunt.model.Item;
+
 /**
  *
  * @author Ann
  */
 public class ItemControl {
     
-    
+    public static Item[] createItems() {
+        // created array(list) of items
+        Item[] item = new Item[Constants.NUMBER_OF_ITEMS];
+        
+        Item clueBag = Item.ClueBag;
+        clueBag.setDescription("Clue Bag");
+        clueBag.setQuantityInStock(0);
+        clueBag.setRequiredAmount(0);
+        item[Item.ClueBag.ordinal()] = clueBag;
+        
+        Item monkey = Item.Monkey;
+        monkey.setDescription("Monkey");
+        monkey.setQuantityInStock(0);
+        monkey.setRequiredAmount(0);
+        item[Item.Monkey.ordinal()] = monkey;
+        
+        Item oldCellPhone = Item.OldCellPhone;
+        oldCellPhone.setDescription("Old Cell Phone");
+        oldCellPhone.setQuantityInStock(0);
+        oldCellPhone.setRequiredAmount(0);
+        item[Item.OldCellPhone.ordinal()] = oldCellPhone;
+        
+        Item aidBag = Item.AidBag;
+        aidBag.setDescription("Aid Bag");
+        aidBag.setQuantityInStock(0);
+        aidBag.setRequiredAmount(0);
+        item[Item.AidBag.ordinal()] = aidBag;
+
+        Item tnt = Item.TNT;
+        tnt.setDescription("TNT");
+        tnt.setQuantityInStock(0);
+        tnt.setRequiredAmount(0);
+        item[Item.TNT.ordinal()] = tnt;
+        
+        return item;
+    }
 }
