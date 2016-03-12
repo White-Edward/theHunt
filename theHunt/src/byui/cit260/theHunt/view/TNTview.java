38cb7e4b04cc357b44c0fa43d173d9ae1a79310f
Ryan <Ryan@Funderburk>
2016-03-12 16:30:26 -0600

    2/12/16 Added TNT item menu.

---
+++ theHunt/src/byui/cit260/theHunt/view/TNTview.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.theHunt.view;
+
+/**
+ *
+ * @author Ryan
+ */
+public class TNTview extends View {
+    
+        public TNTview() {
+        super("\n"
+            +"\n---------------------------------------------------------------"
+            +"\n|TNT will let you destroy one riddle and receive the clue. Use wisely only one TNT per game|"
+            +"\n---------------------------------------------------------------"
+            +"\n P - Pick Up"
+            +"\n D - Put Down"
+            +"\n U - Use Phone"    
+            +"\n B - Back"
+            +"\n---------------------------------------------------------------", "Choose an option: ");
+        }
+        
+     
+    @Override
+    public boolean doAction(String value) {
+        char choice = value.charAt(0);
+        switch (choice) {
+            case 'P': // pick up cell phone
+                this.startPickUpTNT();
+                break;
+            case 'D': // put down cell phone
+                this.startDropTNT();
+                break;
+            case 'U': // use the old cell phone
+                this.startUseTNT();
+                break;    
+            case 'B': // go back
+                return true;
+            default:
+                System.out.println("\n*** Invalid selection*** Try again");
+                break;
+        }
+        return false;
+    }
+    
+    private void startPickUpTNT() {
+         System.out.println("*** startPickupOldCellPhone function called ***");
+    }
+    
+    private void startDropTNT(){       
+        System.out.println("*** startDropOldCellPhone function called ***");
+    }   
+
+    private void startUseTNT() {
+        System.out.println("*** startUseOldCellPhone function called ***");
+    }
+
+
+  }
+  
+
