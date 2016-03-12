11c480f7a717a2df2de7d9c566d141fae4c7ac0d
Ann Lloyd <annj456@gmail.com>
2016-03-11 20:14:42 -0700

    3/11/2016 - Added Constants class to store constant variables, including NUMBER_OF_ITEMS and RIDDLES (riddles is meant to store all the riddles we come up with, used in generating riddle type questions)

---
+++ theHunt/src/byui/cit260/theHunt/model/Constants.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.theHunt.model;
+
+/**
+ *
+ * @author Ann
+ */
+public class Constants {
+    public final static int NUMBER_OF_ITEMS = 5;
+    public final static String[][] RIDDLES = 
+    {
+        {
+            "Riddle 1",
+            "Riddle 2",
+            "Riddle 3",
+            "Riddle 4",
+            "Riddle 5",
+        },
+        {
+            "Answer 1",
+            "Answer 2",
+            "Answer 3",
+            "Answer 4",
+            "Answer 5",
+        }
+    };
+    
+}
