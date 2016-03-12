9ce4c663af470e8cc966b1bccebd6bd66ef38a29
Ann Lloyd <annj456@gmail.com>
2016-03-11 20:19:18 -0700

    3/11/2016 - Removed public enum Item, because that's defined in the model layer in the Item.java class, right?...

--- theHunt/src/byui/cit260/theHunt/control/ItemControl.java
+++ theHunt/src/byui/cit260/theHunt/control/ItemControl.java
@@ -11,13 +11,5 @@ package byui.cit260.theHunt.control;
  */
 public class ItemControl {
     
-    public enum Item {
-        
-        ClueBag,
-        Monkey,        
-        OldCellPhone,
-        AidBag,
-        TNT;
-    }
     
 }
