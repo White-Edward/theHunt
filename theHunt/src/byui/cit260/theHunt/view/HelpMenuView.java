3fd9facbd8e2a2f273745e56feba21695ffc1d95
Ann Lloyd <annj456@gmail.com>
2016-03-25 20:56:18 -0600

    Added toUpperCase on doAction value (moved from inherited getInput function)

--- theHunt/src/byui/cit260/theHunt/view/HelpMenuView.java
+++ theHunt/src/byui/cit260/theHunt/view/HelpMenuView.java
@@ -25,7 +25,7 @@ public class HelpMenuView extends View {
 
     @Override
     public boolean doAction(String value) {
-        char choice = value.charAt(0);
+        char choice = value.toUpperCase().charAt(0);  // get the first character in the string, change to uppercase
         switch (choice) {
             case 'M': // create and start a new game
                 this.startMoveHelp();
